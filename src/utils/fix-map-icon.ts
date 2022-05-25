dab198b44bc754fa9ce10ce2d6316666c996ec4b
Jakub Świerczek <swierczekjakub1@gmail.com>
2022-05-25 22:24:50 +0200

    Default icon refactored.

--- src/utils/fix-map-icon.ts
+++ src/utils/fix-map-icon.ts
@@ -1,13 +1,14 @@
 import L from 'leaflet';
-
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-L.Marker.prototype.options.icon = L.icon({
+const DefaultIcon = L.icon({
     iconUrl: icon,
     iconRetinaUrl: iconRetina,
     shadowUrl: iconShadow,
-    iconSize: [25,41],
-    iconAnchor: [12,41],
-})
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
