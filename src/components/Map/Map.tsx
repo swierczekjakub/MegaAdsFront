d3411082e3ff04f94a1f4baf2cba95c72ab2ba2b
Jakub Świerczek <swierczekjakub1@gmail.com>
2022-05-30 19:42:18 +0200

    Map and markers are finished.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,38 +1,44 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 import '../../utils/fix-map-icon';
+import {SearchContext} from "../../contexts/search.context";
+import {SimpleAdEntity} from 'types';
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
-import {SearchContext} from "../../contexts/search.context";
+import {SingleAdd} from "./SingleAdd";
 
 export const Map = () => {
-    const {search} = useContext(SearchContext)
+    const {search} = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
 
     useEffect(() => {
-        console.log('Make request to search for', search);
+        (async () => {
+
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`);
+            const data = await res.json();
+
+            setAds(data);
+        })();
     }, [search]);
 
     return (
         <div className="map">
-            <h1>Search for: {search}</h1>
             <MapContainer center={[50.2656066, 18.9938998]} zoom={20}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> & contributors'
                 />
-                <Marker position={[50.2656066, 18.9938998]}>
-                    <Popup>
-                        <h2>IT.focus</h2>
-                        <p>Super firma co programuje.</p>
-                    </Popup>
-                </Marker>
-                <Marker position={[50, 19]}>
-                    <Popup>
-                        <h2>Nie IT FOCUS</h2>
-                        <p>To nie jest IT focus</p>
-                    </Popup>
-                </Marker>
+
+                {
+                    ads.map(ad => (
+                        <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                            <Popup>
+                                <SingleAdd id={ad.id}/>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
             </MapContainer>
         </div>
     );
---
+++ src/components/Map/SingleAdd.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import { AdEntity } from 'types';
+
+interface Props {
+    id: string;
+}
+
+export const SingleAdd = (props: Props) => {
+    const [ad, setAd] = useState<AdEntity | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`http://localhost:3001/ad/${props.id}`);
+            const data = await res.json();
+
+            setAd(data);
+        })();
+    }, []);
+
+    if (ad === null) {
+        return  <p>Wczytywanie...</p>;
+    }
+
+    return <>
+        <h2>{ad.name}</h2>
+        <p>{ad.description}</p>
+        {!!ad.price && <p><b>{ad.price}</b> zł</p>}
+        <hr/>
+        <a href={ad.url} target="_blank">Otwórz ogłoszenie</a>
+    </>
+
+}
